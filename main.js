a93cb3f25657a95e2944187406f1cf9c4e351103
nathan <nathavieiragg@outlook.com>
2026-02-10 04:00:17 -0300

    Commit 1.2

---
+++ main.js
@@ -0,0 +1,10 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "bx bx-x" : "bx bx-menu");
+})
